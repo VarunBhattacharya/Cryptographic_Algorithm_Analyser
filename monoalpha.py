cbb0f8b21fe5eef86cc32f6cf8d2fd06c0d8c277
KetanAgrawal2002 <98745010+KetanAgrawal2002@users.noreply.github.com>
2022-08-13 19:33:40 +0530

    Substitution Algorithm Monoalphabetic

---
+++ monoalpha.py
@@ -0,0 +1,62 @@
+from operator import invert
+
+
+class monoAlphabetic:
+    def __init__(self,message):
+        self.message = message        
+            
+    monoAlphabeticKey = {
+            'A': 'N',
+            'B': 'M',
+            'C': 'B',
+            'D': 'V',
+            'E': 'C',
+            'F': 'X',
+            'G': 'Z',
+            'H': 'A',
+            'I': 'S',
+            'J': 'F',
+            'K': 'D',
+            'L': 'G',
+            'M': 'H',
+            'N': 'K',
+            'O': 'J',
+            'P': 'L',
+            'Q': 'P',
+            'R': 'O',
+            'S': 'I',
+            'T': 'Y',
+            'U': 'U',
+            'V': 'T',
+            'W': 'R',
+            'X': 'W',
+            'Y': 'E',
+            'Z': 'Q',
+        }
+    
+    def encryptWithMonoalphabetic(self):
+        self.message = self.message.upper()
+        encryptedMessage = []
+        for letter in self.message:
+            encryptedMessage.append(self.monoAlphabeticKey.get(letter, letter))   
+        return ''.join(encryptedMessage)
+    
+    def inverseMonoalphaKey(monoAlphabeticKey):
+        inverseKey = {}
+        for key, value in monoAlphabeticKey.items():
+            inverseKey[value] = key
+        return inverseKey
+    
+    decryptionKey = inverseMonoalphaKey(monoAlphabeticKey)
+            
+    def decryptWithMonoalphabetic(self):
+        self.message = self.message.upper()
+        decryptedMessage = []
+        for letter in self.message:
+            decryptedMessage.append(self.decryptionKey.get(letter, letter))
+        return ''.join(decryptedMessage)
+        
+
+
+
+
